23c625741cd910ffc60d1101d9d6a5bf5f075d5f
Bruno Martins <bgcngm@gmail.com>
2019-02-24 14:40:37 +0000

    raphael: Bring up initial vendor sepolicy rules
    Change-Id: Iaf298cb02696dbe8d83b9040011bd93889cf18ad

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,6 +155,7 @@ VENDOR_SECURITY_PATCH := 2018-08-05
 # Sepolicy
 include device/qcom/sepolicy/sepolicy.mk
 
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Treble
