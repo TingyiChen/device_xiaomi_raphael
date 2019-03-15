7d7312ebe9a9c1f42fe430aba5a969fe0ba7ce64
Bruno Martins <bgcngm@gmail.com>
2019-03-15 17:59:36 +0000

    raphael: Create manifest file
    Change-Id: I1c4529f48c5b0c3f7a10a167afd033e070131bae

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,6 +94,7 @@ TARGET_USES_ION := true
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Partitions
