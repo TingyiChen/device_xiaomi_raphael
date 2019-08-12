9f61f933849518e6428a0b7dac4f898d0a17cd00
AndyCGYan <GeForce8800Ultra@gmail.com>
2019-08-12 10:36:24 +0000

    raphael: Add FOD permission
    Change-Id: Id44d26a030ecb2620ef78a4615c7411acf9ef429

--- device.mk
+++ device.mk
@@ -57,6 +57,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_raphael
+PRODUCT_COPY_FILES += \
+    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 
 # HIDL
 PRODUCT_PACKAGES += \
