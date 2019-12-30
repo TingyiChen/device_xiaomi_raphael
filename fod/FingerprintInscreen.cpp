bb83bbc05dc08bf0e0b3d40781464e3debeaf233
Demon000 <demonsingur@gmail.com>
2019-12-31 00:59:10 +0100

    raphael: fod: Remove onRelase calls duplication
    Change-Id: I90f9b0a883cfed516998652cf95f4424c8e06b2b

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -116,8 +116,6 @@ Return<void> FingerprintInscreen::onShowFODView() {
 
 Return<void> FingerprintInscreen::onHideFODView() {
     set(FOD_STATUS_PATH, FOD_STATUS_OFF);
-    set(DISPPARAM_PATH, DISPPARAM_FOD_BACKLIGHT_RESET);
-    xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_NONE);
     return Void();
 }
 
